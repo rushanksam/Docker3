442b18aa87b8d86f88198cdcda3c4b4df2708c3b
Rushank <sambrekarrushank@gmail.com>
2024-03-29 10:29:53 +0530

    first commit

---
+++ src/main/java/com/example/Docker3/Docker3Application.java
@@ -0,0 +1,13 @@
+package com.example.Docker3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Docker3/Docker3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Docker3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
